1a59c33d6b7c03b5dd43c5dfd1fa5502316e62de
Shena Yoshida <sheyoshida@gmail.com>
2016-02-16 15:29:06 -0500

    added more

--- coding-questions.playground/Contents.swift
+++ coding-questions.playground/Contents.swift
@@ -41,17 +41,12 @@ func isPermutation(str1: String, str2: String) -> Bool {
     let array1 = Array(str1.characters)
     let array2 = Array(str2.characters)
   
-    if array1.count != array2.count {
-        return false
-    } else {
-        return  array1.sort(<) == array2.sort(<) // compare arrays in abc order
-    }
+    return  array1.sort(<) == array2.sort(<) // sort and compare arrays in abc order
 }
 
-isPermutation("cat", str2: "tac")
+isPermutation("cat", str2: "tac") // test it!
 isPermutation("cat", str2: "dog")
 isPermutation("caat", str2: "tacc")
-isPermutation("cat", str2: "at")
 
 
 
@@ -65,7 +60,6 @@ swapSpace("Hello, World!")
 
     
 // remove duplicate values from array (disregard final order)
-
 func removeDup(num: [Int]) -> [Int] {
     
     let setNum = Set(num)
@@ -76,8 +70,8 @@ func removeDup(num: [Int]) -> [Int] {
 removeDup([42, 31, 22, 22, 55, 44, 44])
 
 
-// remove duplicate values from array and return sorted array
 
+// remove duplicate values from array and return sorted array
 func removeDupAndSort(num: [Int]) -> [Int] {
     
     let setNum = Set(num)
@@ -90,7 +84,6 @@ removeDupAndSort([42, 31, 22, 22, 55, 44, 44])
 
 
 // remove the duplicate values from an array, preserve original order of array
-
 func removeDuplicates(num: [Int]) -> [Int] {
     
     var setNum = Set<Int>() // create empty set and array
@@ -109,12 +102,21 @@ removeDuplicates([42, 31, 22, 22, 55, 44, 44])
 
 
 // reverse an integer
-
 func reverseInt(num: Int) -> Int {
 
+    var intArr: Array<Int> = [] // placeholder array
+    
+    var numStr = String(num)
+    var strArr = Array(numStr.characters)
+    
+    for item in strArr {
+//        intArr.insert(item, atIndex: 0)
+    }
     
     return 0
 }
+reverseInt(543)
+
 
 
 // given a string, write a function to see if it is a permutation of a palindrome
@@ -133,7 +135,7 @@ func manyPermutation(str1: String, str2: String) -> Bool {
 manyPermutation("taco cat", str2: "atco cta")
 
 
-// determine n!
+// return n!
 func factorial(num: Int) -> Int {
     if num == 0 {
         return 1
@@ -142,21 +144,108 @@ func factorial(num: Int) -> Int {
 }
 factorial(3)
 
+
+
+// what is runtime for swift's array.sort(<)
+// Answer: O(n log n)
+
+
+
 // create function to call the previous function
+func sayHello() {
+    print("oh, hello!")
+}
 
-// implement a binary tree
+func talkingToMyComputer(greeting: String){
+    let myGreetings = ["hello", "hi", "salutations", "bonjour", "salut", "konichiwa"]
+    
+    for word in myGreetings {
+        if greeting == word {
+            sayHello()
+        }
+    }
+}
+talkingToMyComputer("hi")
 
-// what is a grand dispatch queue?
 
-// what is merge sort
-
-// what is difference between merge/bubble sort
 
 // fizzbuzz
+func fizzBuzz(min: Int, max: Int) {
+    
+    for number in min...max {
+         let result = (number % 3, number % 5)  // tuples are so swifty!
+        
+        switch result {
+        case (0, 0):
+            print("FizzBuzz")
+        case (0, _):
+            print("Fizz")
+        case (_, 0):
+            print("Buzz")
+        default:
+            print("\(number)")
+        }
+    }
+}
+fizzBuzz(1, max: 100)
 
-// tic tac toe
 
-// what is runtime for built in swift array.sort(<)
+
+// given an array, return the minimum value
+
+func minValue(var numbers: [Int]) -> Int {
+    
+//    numbers = numbers.sort() // 1) if we can use a sort function: O(n log n)
+//    numbers.minElement() // 2) or just ask for min element: O(n)
+//    let minNum = numbers.reduce(Int.max, combine: { min($0, $1)}) // 3) or use reduce
+    
+    for i in 0...numbers.count-1 {
+        for j in 0...numbers.count-1 { // O(n^2)
+            
+            if numbers[i] < numbers[j] {
+//              swap(&numbers[i], &numbers[j]) // 4) swap is cool!
+                let temp = numbers[i] // bubble sort: O(n^2)
+                numbers[i] = numbers[j]
+                numbers[j] = temp
+            }
+        }
+    }
+    return numbers[0]
+}
+minValue([10, 5, 9, 7, 3, 2, 37, 4])
+
+
+
+// given an array, return min and max values:
+func minAndMax(var num: [Int]) -> [Int] {
+    
+    var minElementAndMaxElement: Array<Int> = [] // new array to return values
+    
+    
+    for i in 0...num.count-1 { // 1) sort the array
+        for j in 0...num.count-1 { // bubble sort: O(n^2)
+            if num[i] < num[j] {
+                let temp = num[i]
+                num[i] = num[j]
+                num[j] = temp
+            }
+        }
+    }
+    
+    let firstItem = num.first
+    let lastItem = num.last
+    
+    minElementAndMaxElement.append(firstItem!) // 2) add first and last items to array
+    minElementAndMaxElement.append(lastItem!)
+
+    return minElementAndMaxElement
+}
+minAndMax([5, 3, 7, 9, 21, 2])
+
+
+
+
+
 
 // take in string, return letter that appears the most frequently
 //func mostCommonLetter(word: String) -> String? {
@@ -188,7 +277,44 @@ factorial(3)
 //mostCommonLetter("heellloe")
 
 
+/*
+adding / multiplying / traversing multi dimensional arrays
+caesar cypher
+tic tac toe
+Check to see if string is balanced (())
+Given three numbers return the median
+Fibonacci - given an idx return the fib value at idx
+given a jumble: String and knownWords: [String] return list of potential words that the string may be.
+given a string, return the letter that appears most frequently
 
+Given two int arrays v1 and v2 return 1 if  v1 is greater  -1 if v2 is greater and 0 if they are equal. V1 and V2 are like software versions
+
+Given two numbers add them up and return an array or string version of the result ie: 5 + 12 = [1,7] or “17”
+
+how many golf balls could you fit in a school bus?
+
+Given an amount of money to convert to change. Write an algorithm that returns the optimal arrangement of change, that is, an array with the values of the coins returned to the user. The optimal array contains the least number of coins needed for the arrangement (think vending machine).
+
+You are given an unsorted array containing the numbers 1-100 inclusive and one additional random number.  Find the value of that number
+
+Given an array of integers, determine if the array is in sorted order.
+
+Sort an algorithm in n^2 time.  Attempt to write the algorithm from scratch
+
+Sort an algorithm in nlog(n) time.  Attempt to write the algorithm from scratch
+
+Given a non-negative int n, return the sum of its digits recursively (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).  (from http://codingbat.com/prob/p163932)
+
+Given two arrays of integers, compute the pair of values (one value in each array) with the smallest (non-negative) difference.  Return the difference. (from Cracking the Coding Interview p. 181)
+
+EXAMPLE:
+Input:{1,3,15,11,2},{23,127,235,19,8}
+Output: 3 (because 11-8=3)
+
+Design an algorithm to identify if a player has won a game of tic-tac-toe (from Cracking the Coding Interview p. 181)
+
+
+*/
 
 
 
